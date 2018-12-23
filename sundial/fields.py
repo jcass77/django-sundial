2b09a16e5378527e022f6fd51711ff7f1b8371ec
jcass77 <john.cass77@gmail.com>
2018-12-23 07:51:05 +0200

    Resolve RemovedInDjango30Warning deprecation warning.

--- sundial/fields.py
+++ sundial/fields.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import django
 from django.db import models
 from django.utils.encoding import force_text
 
@@ -15,10 +16,19 @@ class TimezoneField(models.CharField):
         kwargs.setdefault('max_length', self.default_max_length)
         super(TimezoneField, self).__init__(*args, **kwargs)
 
-    def from_db_value(self, value, expression, connection, context):
-        if value:
-            value = coerce_timezone(value)
-        return value
+    # 'context' argument was deprecated in Django 2.0
+    # See: https://docs.djangoproject.com/en/2.0/releases/2.0/#features-deprecated-in-2-0
+    if django.VERSION < (2,):
+        # TODO: remove when Django<2 support is dropped.
+        def from_db_value(self, value, expression, connection, context):
+            if value:
+                value = coerce_timezone(value)
+            return value
+    else:
+        def from_db_value(self, value, expression, connection):
+            if value:
+                value = coerce_timezone(value)
+            return value
 
     def to_python(self, value):
         value = super(TimezoneField, self).to_python(value)
